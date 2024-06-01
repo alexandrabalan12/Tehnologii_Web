d9004f49a4c6ed29d79a2478b76373df05c784a4
andraroman12 <andra.roman11@yahoo.com>
2024-06-01 14:41:41 +0300

    .

---
+++ frontend/scripts/navbar.js
@@ -0,0 +1,185 @@
+// Navbar templates
+const navbarNotLogged = `
+    <nav class="navbar">
+        <div class="navbar-container">
+            <a href="index.html" class="logo1">PlaCo</a>
+            <input type="checkbox" class="navigation-mobile-toggle" id="navi-toggle">
+            <label class="navbar-mobile-button" for="navi-toggle">
+                <img src="./assets/nav-menu.png" alt="Menu" />
+            </label>
+            <div class="nav-menu" id="navbar-menu">
+                <a href="login.html" class="nav-link">Available projects</a>
+                <a href="aboutus.html" class="nav-link">About us</a>        
+                <a href="login.html" class="nav-link login">Login</a>
+                <a href="signup.html" class="signup-btn">Sign up</a>
+            </div>   
+        </div>
+    </nav>
+`;
+
+const navbarClient = `
+    <nav class="navbar">
+        <div class="navbar-container">
+            <a href="index.html" class="logo1">PlaCo</a>
+            <input
+                type="checkbox"
+                class="navigation-mobile-toggle"
+                id="navi-toggle"
+            />
+            <label class="navbar-mobile-button" for="navi-toggle">
+                <img src="./assets/nav-menu.png" alt="Menu" />
+            </label>
+            <div class="nav-menu" id="navbar-menu">
+                <!-- continut navigatie -->
+                <a href="./your-projects-client.html" class="nav-link">Your projects</a>
+                <a href="aboutus.html" class="nav-link">About us</a>
+                <div class="nav-search input-group__area">
+                    <img src="./assets/icon_search.png" />
+                    <input
+                        id="navbar-search"
+                        name="navbar-seach"
+                        type="text"
+                        placeholder="Search for companies"
+                    />
+                </div>
+                <button id="logout-button" class="signup-btn">Log out</button>
+            </div>
+        </div>
+    </nav>
+`;
+
+const navbarCompany = `
+    <nav class="navbar">
+        <div class="navbar-container">
+            <a href="index.html" class="logo1">PlaCo</a>
+            <input
+                type="checkbox"
+                class="navigation-mobile-toggle"
+                id="navi-toggle"
+            />
+            <label class="navbar-mobile-button" for="navi-toggle">
+                <img src="./assets/nav-menu.png" alt="Menu" />
+            </label>
+            <div class="nav-menu" id="navbar-menu">
+                <!-- continut navigatie -->
+                <a href="availableprojects.html" class="nav-link">Available projects</a>
+                <a href="aboutus-afterlogin.html" class="nav-link">About us</a>
+                <div class="nav-search input-group__area">
+                    <img src="./assets/icon_search.png" />
+                    <input
+                        id="navbar-search"
+                        name="navbar-seach"
+                        type="text"
+                        placeholder="Search for projects"
+                    />
+                </div>
+                <a href="myaccount-company.html">
+                    <img src="./assets/profile.png" />
+                </a>
+                <button id="logout-button" class="signup-btn">Log out</button>
+            </div>
+        </div>
+    </nav>
+`;
+
+document.addEventListener("DOMContentLoaded", async () => {
+  // variabile
+  let user_data = null;
+
+  const cached_data = JSON.parse(localStorage.getItem("userData") || "{}");
+
+  if (cached_data && cached_data.role) {
+    user_data = cached_data;
+    user_role = cached_data.role;
+  }
+
+  // definim functii
+  function handleSearch() {
+    const input = document.getElementById("navbar-search");
+    const userType = user_data.role;
+
+    input.addEventListener("keypress", (event) => {
+      if (event.key === "Enter") {
+        const encodedQuery = encodeURIComponent(input.value);
+        window.location.href = `http://127.0.0.1:5500/${userType}-search.html?query=${encodedQuery}`;
+      }
+    });
+  }
+
+  function routeGuard() {
+    return;
+    // protejam rutele de 'client' si 'company';
+    const route = window.location.pathname.slice(1);
+    const type = route.split("-")[0];
+
+    if (type === "client" || type === "company") {
+      if (user_data.role !== type) {
+        window.location.href = "http://127.0.0.1:5500/login.html";
+      }
+    }
+  }
+
+  function handleLogout() {
+    const logout_button = document.getElementById("logout-button");
+
+    logout_button.addEventListener("click", () => {
+      localStorage.removeItem("authToken");
+      localStorage.removeItem("userData");
+      window.location.href = "http://127.0.0.1:5500";
+    });
+  }
+
+  function setNavbar() {
+    // setam navbar in functie de user
+    const navbar_container = document.getElementById("navbar-container");
+
+    if (user_data && user_data.role === "client") {
+      navbar_container.innerHTML = navbarClient;
+      handleSearch();
+      handleLogout();
+    } else if (user_data && user_data.role === "company") {
+      navbar_container.innerHTML = navbarCompany;
+      handleSearch();
+      handleLogout();
+    } else navbar_container.innerHTML = navbarNotLogged;
+
+    console.log(navbar_container);
+  }
+
+  async function getUserData() {
+    const token = localStorage.getItem("authToken");
+
+    if (!token) return;
+
+    const response = await apiRequest(
+      "http://localhost:8000/api/user-data",
+      "GET",
+      null,
+      {
+        authorization: token,
+      }
+    );
+
+    user_data = response.user;
+
+    if (!user_data) {
+      // userul nu e valid
+      // stergem datele
+      localStorage.removeItem("authToken");
+      localStorage.removeItem("userData");
+    } else {
+      // pastram datele sa fie si pt celelalte pagini
+      localStorage.setItem("userData", JSON.stringify(user_data));
+    }
+
+    setNavbar();
+    routeGuard();
+  }
+
+  // apelam functii
+
+  // setam navbar initial
+  setNavbar();
+
+  await getUserData();
+});
---
+++ frontend/scripts/utilites.js
@@ -0,0 +1,27 @@
+async function apiRequest(url, method = "GET", payload = null, headers = {}) {
+  const options = {
+    method: method,
+    headers: {
+      "Content-Type": "application/json",
+      ...headers,
+    },
+  };
+
+  if (payload) {
+    options.body = JSON.stringify(payload);
+  }
+
+  try {
+    const response = await fetch(url, options);
+
+    if (!response.ok) {
+      throw new Error(`HTTP error! status: ${response.status}`);
+    }
+
+    const jsonResponse = await response.json();
+    return jsonResponse;
+  } catch (error) {
+    console.error("Error during the fetch operation:", error);
+    throw error;
+  }
+}
